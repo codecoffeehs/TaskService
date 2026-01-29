591eacf27e452f82567cef92630c4e75110f131d
Hemant Sharma <codecoffeehs@icloud.com>
2026-01-29 15:46:59 +0530

    Added migrations

---
+++ Migrations/20260129101544_AddedDescriptionInTaskModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDescriptionInTaskModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "tasks",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "tasks",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "tasks",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
